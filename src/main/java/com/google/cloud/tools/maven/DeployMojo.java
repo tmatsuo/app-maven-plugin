ce0c863e96deecd0a9fecb5cd72dd4c97d7bfae4
Mike Eltsufin <meltsufin@google.com>
2016-04-28 17:03:13 -0400

    Added deploy and stage mojos

---
+++ src/main/java/com/google/cloud/tools/maven/DeployMojo.java
@@ -0,0 +1,110 @@
+package com.google.cloud.tools.maven;
+
+/**
+ * Stages and then deploys application to App Engine standard or flexible environment.
+ */
+
+import com.google.cloud.tools.app.api.AppEngineException;
+import com.google.cloud.tools.app.api.deploy.AppEngineDeployment;
+import com.google.cloud.tools.app.api.deploy.DeployConfiguration;
+import com.google.cloud.tools.app.impl.cloudsdk.CloudSdkAppEngineDeployment;
+import com.google.cloud.tools.app.impl.cloudsdk.internal.sdk.CloudSdk;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.util.List;
+
+@Mojo(name = "deploy")
+@Execute(phase = LifecyclePhase.PACKAGE)
+public class DeployMojo extends StageMojo implements DeployConfiguration {
+
+  @Parameter(required = true, property = "deploy.deployables")
+  private List<File> deployables;
+  @Parameter(property = "gcp.app.deploy.bucket")
+  private String bucket;
+  @Parameter(property = "gcp.app.deploy.dockerBuild")
+  private String dockerBuild;
+  @Parameter(property = "gcp.app.deploy.force")
+  private boolean force;
+  @Parameter(property = "gcp.app.deploy.imageUrl")
+  private String imageUrl;
+  @Parameter(property = "gcp.app.deploy.promote")
+  private boolean promote;
+  @Parameter(property = "gcp.app.deploy.server")
+  private String server;
+  @Parameter(property = "gcp.app.deploy.stopPreviousVersion")
+  private boolean stopPreviousVersion;
+  @Parameter(property = "gcp.app.deploy.version")
+  private String version;
+
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    // execute stage
+    super.execute();
+
+    if (deployables.size() == 0) {
+      deployables.add(
+          new File(getStagingDirectory() + "/app.yaml"));
+    }
+
+    CloudSdk sdk = new CloudSdk(cloudSdkPath);
+    AppEngineDeployment deployment = new CloudSdkAppEngineDeployment(sdk);
+
+    try {
+      deployment.deploy(this);
+    } catch (AppEngineException e) {
+      throw new MojoExecutionException(e.getMessage());
+    }
+  }
+
+  @Override
+  public List<File> getDeployables() {
+    return deployables;
+  }
+
+  @Override
+  public String getBucket() {
+    return bucket;
+  }
+
+  @Override
+  public String getDockerBuild() {
+    return dockerBuild;
+  }
+
+  @Override
+  public boolean isForce() {
+    return force;
+  }
+
+  @Override
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  @Override
+  public boolean isPromote() {
+    return promote;
+  }
+
+  @Override
+  public String getServer() {
+    return server;
+  }
+
+  @Override
+  public boolean isStopPreviousVersion() {
+    return stopPreviousVersion;
+  }
+
+  @Override
+  public String getVersion() {
+    return version;
+  }
+}
---
+++ src/main/java/com/google/cloud/tools/maven/StageMojo.java
@@ -0,0 +1,199 @@
+package com.google.cloud.tools.maven;
+
+import com.google.cloud.tools.app.api.AppEngineException;
+import com.google.cloud.tools.app.api.deploy.AppEngineFlexibleStaging;
+import com.google.cloud.tools.app.api.deploy.AppEngineStandardStaging;
+import com.google.cloud.tools.app.api.deploy.StageFlexibleConfiguration;
+import com.google.cloud.tools.app.api.deploy.StageStandardConfiguration;
+import com.google.cloud.tools.app.impl.appcfg.AppCfgAppEngineStandardStaging;
+import com.google.cloud.tools.app.impl.appcfg.AppEngineSdk;
+import com.google.cloud.tools.app.impl.cloudsdk.CloudSdkAppEngineFlexibleStaging;
+import com.google.cloud.tools.app.impl.cloudsdk.internal.sdk.PathResolver;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Stages application for App Engine standard or flexible environment deployment.
+ */
+@Mojo(name = "stage")
+@Execute(phase = LifecyclePhase.PACKAGE)
+public class StageMojo extends AbstractMojo implements StageStandardConfiguration,
+    StageFlexibleConfiguration {
+
+  // Standard & Flexible params
+  @Parameter(required = true)
+  protected File cloudSdkPath;
+  @Parameter(required = true, defaultValue = "${project.build.directory}/appengine-staging",
+      property = "gcp.app.stage.stagingDirectory")
+  private File stagingDirectory;
+  @Parameter(property = "gcp.app.stage.dockerfile")
+  private File dockerfile;
+
+  // Standard-only params
+  @Parameter(required = true,
+      defaultValue = "${project.build.directory}/${project.build.finalName}",
+      property = "gcp.app.stage.sourceDirectory")
+  private File sourceDirectory;
+  @Parameter(property = "gcp.app.stage.enable-quickstart")
+  private boolean enableQuickstart;
+  @Parameter(property = "gcp.app.stage.disable-update-check")
+  private boolean disableUpdateCheck;
+  // TODO: application vs project?
+  @Parameter(property = "gcp.app.stage.project")
+  private String applicationId;
+  @Parameter(property = "gcp.app.stage.enable-jar-splitting")
+  private boolean enableJarSplitting;
+  @Parameter(property = "gcp.app.stage.jar-splitting-excludes")
+  private String jarSplittingExcludes;
+  @Parameter(property = "gcp.app.stage.compile-encoding-key")
+  private String compileEncoding;
+  @Parameter(property = "gcp.app.stage.delete-jsps")
+  private boolean deleteJsps;
+  @Parameter(property = "gcp.app.stage.enable-jar-classes")
+  private boolean enableJarClasses;
+  @Parameter(property = "gcp.app.stage.version")
+  protected String version;
+
+  // Flexible-only params
+  @Parameter(defaultValue = "${basedir}/src/main/appengine/app.yaml")
+  private File appYaml;
+  @Parameter(defaultValue =
+      "${project.build.directory}/${project.build.finalName}.${project.packaging}")
+  private File artifact;
+
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    if (cloudSdkPath == null) {
+      try {
+        cloudSdkPath = PathResolver.INSTANCE.getCloudSdkPath();
+      } catch (FileNotFoundException e) {
+        throw new MojoFailureException(
+            "Cloud SDK Path was not provided, and cannot be automatically detected.", e);
+      }
+    }
+
+    // delete staging directory if it exists
+    if (stagingDirectory.exists()) {
+      getLog().info("Deleting the staging directory: " + stagingDirectory);
+      try {
+        FileUtils.deleteDirectory(stagingDirectory);
+      } catch (IOException e) {
+        new MojoFailureException("Unable to delete staging directory.", e);
+      }
+    }
+    stagingDirectory.mkdir();
+
+    try {
+      getLog().info("Staging the application to: " + stagingDirectory);
+
+      if (new File(sourceDirectory.toString() + "/WEB-INF/appengine-web.xml").exists()) {
+        getLog().info("Detected App Engine standard environment application.");
+        stageStandard();
+      } else {
+        getLog().info("Detected App Engine flexible environment application.");
+        stageFlexible();
+      }
+
+    } catch (AppEngineException e) {
+      throw new MojoFailureException("Staging for deployment failed.", e);
+    }
+
+  }
+
+  private void stageStandard() throws AppEngineException {
+    AppEngineSdk appEngineSdk = new AppEngineSdk(new File(cloudSdkPath.toString()
+        + "/platform/google_appengine/google/appengine/tools/java"));
+
+    AppEngineStandardStaging staging = new AppCfgAppEngineStandardStaging(appEngineSdk);
+
+    // execute the staging
+    staging.stageStandard(this);
+  }
+
+  private void stageFlexible() throws AppEngineException {
+    getLog().info(artifact.toString());
+    AppEngineFlexibleStaging staging = new CloudSdkAppEngineFlexibleStaging();
+    staging.stageFlexible(this);
+  }
+
+  @Override
+  public File getSourceDirectory() {
+    return sourceDirectory;
+  }
+
+  @Override
+  public File getStagingDirectory() {
+    return stagingDirectory;
+  }
+
+  @Override
+  public File getDockerfile() {
+    return dockerfile;
+  }
+
+  @Override
+  public boolean isEnableQuickstart() {
+    return enableQuickstart;
+  }
+
+  @Override
+  public boolean isDisableUpdateCheck() {
+    return disableUpdateCheck;
+  }
+
+  @Override
+  public String getApplicationId() {
+    return applicationId;
+  }
+
+  @Override
+  public boolean isEnableJarSplitting() {
+    return enableJarSplitting;
+  }
+
+  @Override
+  public String getJarSplittingExcludes() {
+    return jarSplittingExcludes;
+  }
+
+  @Override
+  public String getCompileEncoding() {
+    return compileEncoding;
+  }
+
+  @Override
+  public boolean isDeleteJsps() {
+    return deleteJsps;
+  }
+
+  @Override
+  public boolean isEnableJarClasses() {
+    return enableJarClasses;
+  }
+
+  @Override
+  public String getVersion() {
+    return version;
+  }
+
+  @Override
+  public File getAppYaml() {
+    return appYaml;
+  }
+
+  @Override
+  public File getArtifact() {
+    return artifact;
+  }
+}
